67f7bffbffc6cb055faf4128bcee3801a34e4198
hapo-hieptv <hieptv@haposoft.com>
2025-05-30 13:41:19 +0700

    create refresh token

--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -2,9 +2,11 @@
 
 from src.db.models.user import User
 from src.db.models.role import Role
+from src.db.models.token import RefreshToken
 
 # Export all models
 __all__ = [
     "User",
     "Role",
+    "RefreshToken"
 ]
